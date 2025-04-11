93fcea82539028ec97d0f207795f270097abd6ec
O'tkir <keldibekovotkir767@gmail.com>
2025-04-11 21:49:41 +0500

    first

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -26,8 +26,9 @@ function HeroSection() {
             Hello, <br />
             This is {' '}
             <span className=" text-pink-500">{personalData.name}</span>
-            {` , I'm a Professional `}
+            {` , I&apos;m a Professional `}
             <span className=" text-[#16f2b3]">{personalData.designation}</span>
+
             .
           </h1>
 
